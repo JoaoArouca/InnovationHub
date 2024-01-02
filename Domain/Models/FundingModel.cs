3166495e4ced02a66bd9660d082ea206423c8842
JoaoArouca <joao.silveira@estudante.ufla.br>
2024-01-01 22:58:49 -0300

    feat: create funding Model (initial structure)

---
+++ Domain/Models/FundingModel.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Models.Auxiliary;
+
+namespace Domain.Models
+{
+    public class FundingModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public List<CountryModel> Countries { get; set; }
+        public List<OrganizationModel> Organizations { get; set; }
+        public List<SectorModel> Sectors { get; set; }
+        public List<TechnologyModel> Technologies { get; set; }
+        public List<PartnerTypeModel> PartnerTypes { get; set; }
+        public FundingModel
+        (
+            string title,
+            List<CountryModel> countries,
+            List<OrganizationModel> organization,
+            List<SectorModel> sectors,
+            List<TechnologyModel> technologies,
+            List<PartnerTypeModel> partnerTypes
+        )
+        {
+            Id = Guid.NewGuid();
+            Title = title;
+            Countries = countries;
+            Organizations = organization;
+            Sectors = sectors;
+            Technologies = technologies;
+            PartnerTypes = partnerTypes;
+        }
+
+    }
+}
