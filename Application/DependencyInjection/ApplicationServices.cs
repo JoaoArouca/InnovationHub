a3065d08c1aed0acc34033275f4c7cfc0767d8ea
JoaoArouca <joao.silveira@estudante.ufla.br>
2024-01-02 22:36:56 -0300

    feat: create dependency injection and dbContext

---
+++ Application/DependencyInjection/ApplicationServices.cs
@@ -0,0 +1,19 @@
+﻿using Application.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.DependencyInjection
+{
+    public static class ApplicationServices
+    {
+        public static IServiceCollection AddInMemoryDatabase(this IServiceCollection services)
+        {
+            services.AddDbContext<InMemoryDatabase>();
+            return services;
+        }
+
+        //public static IServiceCollection AddValidators(this IServiceCollection services)
+        //{
+        //    services.AddTransient<IModelValidator<input>, output>().
+        //}
+    }
+}
---
+++ Application/Repositories/InMemoryDatabase.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models.Auxiliary;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Repositories
+{
+    public class InMemoryDatabase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase(databaseName: "inMemoryDatabase");
+        }
+
+        public DbSet<CountryModel> CountriesDB { get; set; }
+    }
+}
