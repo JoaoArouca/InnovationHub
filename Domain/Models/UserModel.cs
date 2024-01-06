d30f7110f68aba142bba6109f4dbee8ed51d1d8c
JoaoArouca <jarouca21@gmail.com>
2024-01-06 16:25:50 -0300

    feat: create user model

---
+++ Domain/Models/UserModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Domain.Models
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public UserModel(Guid id, string name, string email, string password, Role role)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+            Role = role;
+            CreatedAt = DateTime.Now;
+        }
+    }
+
+    public enum Role
+    {
+        CUSTOMER,
+        ADMIN,
+        INTERNAL
+    }
+}
